b60a14bdbe832bfc1915792e7d63943c6a4e3e05
Kheya <kheyadas1919@gmail.com>
2023-07-15 12:32:16 +0530

    navbar and herosection added

---
+++ src/app/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import styles from "./Hero.module.css";
+import earth from "../../../../public/images/earth.svg";
+import satellite from "../../../../public/images/satellite.svg";
+
+function HeroSection() {
+  return (
+    <div className={`${styles.heroSection}`}>
+      <Image src={earth} alt="earth" className={`${styles.heroimgearth}`} />
+      <Image
+        src={satellite}
+        alt="satellite"
+        className={`${styles.heroimgsatellite}`}
+      />
+      <h1 className={`${styles.heading}`} style={{ marginBottom: "-16px" }}>
+        Electronics & Communication
+      </h1>
+      <h1 className={`${styles.heading}`}> Engineering Society</h1>
+
+      <p className={`${styles.subtext}`}>
+        We are a team , an organisation, a family, a society.
+      </p>
+    </div>
+  );
+}
+
+export default HeroSection;
