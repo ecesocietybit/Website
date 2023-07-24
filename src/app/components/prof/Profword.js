966fd21a38c530208df5f7572efad10612190137
naraynabhay <abhaynarayan07062002@gmail.com>
2023-07-24 11:46:15 +0530

    added css of profword

--- src/app/components/prof/Profword.js
+++ src/app/components/prof/Profword.js
@@ -2,11 +2,19 @@ import React from 'react'
 import style from './Profword.module.css'
 import { ProfData } from "@/lib/data/ProfData";
 import ProfDetail from '../profdetail/ProfDetail';
+import StyledText from '../text/StyledText'
 const Profword = () => {
     // console.log(ProfData);
   return (
     <div>
-       Few Inspiring Words
+      {/* <p className={`${style.word} text-center  `}> Ours Professors</p> */}
+      <StyledText
+          primary="#FF4D4D"
+          secondary="#F9CB28"
+          className="text-[36px] font-bold flex flex-col items-center mb-4"
+        >
+         Ours Professors
+        </StyledText>
       {
        ProfData.map((items)=>{
         {console.log(items.imgsrc);}
@@ -16,7 +24,11 @@ const Profword = () => {
         imgsrc={items.imgsrc}
         name={items.name}
         pos={items.pos}
-        words={items.word}/>
+        words={items.word}
+        college={items.college}
+        dept={items.dept}
+        socials={items.socials}/>
+        
         )
        })
     }
--- src/app/components/profdetail/ProfDetail.js
+++ src/app/components/profdetail/ProfDetail.js
@@ -1,27 +1,121 @@
+"use client"
 import Image from 'next/image';
-import React from 'react'
+import React, { useState } from 'react'
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
+import {AiFillInstagram} from 'react-icons/ai'
 import style from './ProfDetail.module.css'
+import StyledText from '../text/StyledText'
 
-const ProfDetail = ({imgsrc,name,pos,words}) => {
+const ProfDetail = ({imgsrc,name,pos,words,college,dept,socials}) => {
+  const [showFullText, setShowFullText] = useState(false);
+  let maxLength=250;
+  const ICON_SIZE = 28;
+
+  const handleReadMoreClick = (name) => {
+    setShowFullText(!showFullText);
+  };
+
     console.log(pos);
   return (
-    <div className={`${style.header} flex `}>
+    <div className={`${style.header} flex  text-[#fff] `}>
       
     <div className={`${style.left} flex  flex-col`}>
      <div className={`${style.container} `}>
      <Image className={`${style.img} `}
-       height={600}
-       width={600}
+       height={100}
+       width={100}
         alt={name} 
         src={imgsrc}
       /> 
      </div>
-      <p>{pos}</p>
+     <div className={`${style.leftDown}`}>
+     <p className={`${style.pos} `}>{pos}</p>
+      <div className={`${style.socials}`}>
+        {
+          socials.github||socials.linkedin||socials.email||socials.insta ? (
+            <>
+            {socials.github && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://github.com/${socials.github}/`}
+              >
+                <FaGithub size={ICON_SIZE} width={ICON_SIZE} />
+              </a>
+            )}
+
+            {socials.linkedin && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://www.linkedin.com/in/${socials.linkedin}/`}
+              >
+                <FaLinkedin size={ICON_SIZE} />
+              </a>
+            )}
+            {socials.insta && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://instagram.com/${socials.insta}/`}
+              >
+                <AiFillInstagram size={ICON_SIZE} />
+              </a>
+            )}
+            {socials.email && (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`mailto:${socials.email}`}
+              >
+                <FaEnvelope size={ICON_SIZE} />
+              </a>
+            )}
+          </>
+          ) :(
+            <span>
+              Socially Invisible
+            </span>
+          )
+        }
+      </div>
+     </div>
+ 
     </div>
       
-      <div className={`${style.left} flex  flex-col`}  >
-      <p className={`${style.name} `}>{name}</p>
-      <div className={`${style.word} `}>{words}</div>
+      <div className={`${style.right} flex  flex-col`}  >
+      {/* <p className={`${style.name} `}>{name}</p> */}
+      <StyledText
+          primary="#FF4D4D"
+          secondary="#F9CB28"
+          className="text-[36px] font-bold flex flex-col items-center mb-4"
+        >
+         {name}
+        </StyledText>
+      <div className={`${style.subsidary} `}>
+        <p className={`${style.dept} `}> {dept}</p>
+        <p className={`${style.college} `}>{college}</p>
+      </div>
+
+      <div className={`${style.word} `}>
+        {showFullText ? (
+          <div>
+        <p>{words}</p>
+        <button onClick={()=>{handleReadMoreClick(name)}} className={`${style.readbtn} `}>show less</button>
+          </div>
+        
+      ) : (
+        <div>
+
+        <p>
+          {words.length > maxLength ? `${words.slice(0, maxLength)}...` : words}
+          </p>
+          <button onClick={handleReadMoreClick} className={`${style.readbtn} `}>Read More</button>
+        </div>
+        
+      )}
+      </div>
+
       </div>
     </div>
   )
