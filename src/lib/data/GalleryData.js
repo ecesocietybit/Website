c2e2f3befc144dbe5ab14f256e33e83d849ccec8
Aditya Bhardwaj <aditya.s49@outlook.com>
2023-08-14 02:10:26 +0530

    Glimpses + Loader

--- src/lib/data/GalleryData.js
+++ src/lib/data/GalleryData.js
@@ -2,35 +2,44 @@
     {
         id:1,
         title:'Prakshit',
-        imgSrc:'/gallery/1.jpg',
+        imgSrc:'1.png',
     },
     {
         id:2,
         title:'Shashrath',
-        imgSrc:'/gallery/2.jpg',
+        imgSrc:'2.png',
     },
     {    id:3,
         title: 'Image3',
-        imgSrc:'/gallery/3.jpg'
+        imgSrc:'3.png'
     },
     {
         id:4,
         title: 'Image3',
-        imgSrc:'/gallery/4.jpg'
+        imgSrc:'4.png'
     },
     {    id:5,
         title: 'Image3',
-        imgSrc:'/gallery/5.jpg'
+        imgSrc:'5.png'
     },
     {   
         id:6,
         title: 'Image3',
-        imgSrc:'/gallery/6.jpg'
+        imgSrc:'6.png'
     },
     {   
         id:7,
         title: 'Image3',
-        imgSrc:'/gallery/7.jpg'
+        imgSrc:'7.png'
     },
 
+ ]
+
+ export const AboutTextPara = [
+    {
+        text: `The ECE Society is the official society of the Department of Electronics and Communication Engineering, BIT Sindri. This society aims to promote Scientific thinking among the students and provide a holistic environment where they can freely explore the latest cutting-edge technologies in the field of electronics and Software solutions to real-life problems.
+        Electronics and communication engineering society organizes numerous activities such as tech talks, innovation and theory (seminars), quizzes, problem-solving, graphic designing, and other technical events to enable participants gain new information and passion for the competitive drive.
+        It gives its participants the luxury of portraying themselves as prominent technocrats and corporative individuals. It is accomplished through exposure that comes with innovation and creativity, event management, client interaction, technical skills such as poster creation, model making, graphic design, and working in diverse environments.
+        The ECE Society organizes various webinars, seminars, and Alumni Talks where students learn from the experience of our distinguished Alumni working in diverse fields, ranging from Indian Administration to tech giants like amazon, google, and many more.`
+    }
  ]
