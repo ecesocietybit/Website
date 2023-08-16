062a407acee5ed9ae80805dc96e1a2a69f8ad4d8
Mohit Pandey <pandeymohit215@gmail.com>
2023-08-16 16:21:10 +0530

    image optimisation

--- src/app/components/EventCard/PastCard.js
+++ src/app/components/EventCard/PastCard.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import Styles from "./PastCard.module.css";
+import Image from 'next/image';
 
-const PastCard = ({title,content,imgSrc,link}) => {
+const PastCard = ({title,content,imgSrc,lazyImgSrc,link}) => {
     const bg='/pastevent/event.jpg'
     link=link.split('.')[0];
     link=link+'.pdf';
     // console.log(link);
   return (
-    <div style={{ backgroundImage: `url(${imgSrc})` }} className={Styles.card}>
+    <div className={Styles.card}>
+      <Image
+            className={`h-[101.75px] w-[101.75px] mx-auto object-cover object-top`}
+            fill
+            sizes='(max-width:768px) 100vw,700px'
+            src={imgSrc}
+            placeholder='blur'
+            blurDataURL={lazyImgSrc}
+            alt={title}
+            />
     <div className={Styles.cardBody}>
       <h2 className={Styles.cardTitle}>{title}</h2>
       {/* <div className={Styles.underline}></div> */}
